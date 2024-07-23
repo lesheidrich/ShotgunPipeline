53f7c0b518b2f82453e1fe35e18fc58fe2e29909
Les <lesismorethanles@gmail.com>
2024-07-23 19:54:45 +0200

    initial filter by e-val done

--- main.py
+++ main.py
@@ -5,29 +5,17 @@ from src.taxonomy import Taxonomy
 path = Path.cwd() / 'FASTQ_sample' / 'SRR9855890.fastq.gz'
 threshold = 20
 
+# phreds = [32, 33, 32, 32, 18, 29, 32, 32, 32, 29, 29, 32, 38, 38, 38, 37, 17, 36, 36, 36, 37, 38, 39, 38, 38, 38, 32, 37, 38, 37, 39, 39, 39, 34, 17, 37, 36, 36, 38, 38, 39, 39, 38, 32, 16, 33, 36, 36, 38, 38, 34, 37, 38, 39, 38, 32, 18, 15, 15, 33, 33, 38, 38, 39, 39, 37, 20, 37, 19, 37, 38, 38, 36, 39, 37, 39, 33, 38, 39, 39, 17, 37, 38, 38, 37, 38, 37, 37, 34, 38, 37, 38, 37, 39, 37, 37, 39, 38, 39, 36, 39, 35, 19, 35, 38, 37, 38, 37, 38, 37, 39, 38, 36, 14, 33, 36, 39, 39, 39, 38, 39, 39, 39, 38, 27, 38, 39, 39, 39, 39, 39, 39, 39, 38, 38, 35, 33, 38, 37, 38, 39, 38, 38, 38, 39, 37, 39, 39, 36, 39, 37, 33, 38, 38, 37, 38, 35, 38, 38, 37, 39, 38, 36, 37, 36, 37, 35, 33, 33, 38, 35, 16, 38, 35, 37, 36, 39, 36, 39, 39, 38, 38, 37, 32, 33, 16, 33, 16, 14, 34, 38, 38, 39, 39, 39, 39, 39, 39, 39, 38, 33, 32, 16, 32, 39, 39, 37, 39, 38, 36, 32, 32, 32, 33, 17, 33, 16, 37, 37, 37, 37, 37, 38, 38, 34, 38, 33, 32, 15, 15, 36, 38, 36, 37, 37, 38, 38, 38, 38, 38, 38, 37, 37, 37, 33, 37, 37, 37, 32, 32, 32, 32, 32]
 seq = 'TACGAAGGGTGCAAGCGTTACTCGGAATTACTGGGCGTAAAGCGTGCGTAGGTGGTCGTTTAAGTCTGTTGTGAAAGCCCTGGGCTCAACCTGGGAACTGCAGTGGAAACTGGACGACTAGAGTGTGGTAGAGGGTAGCGGAATTCCTGGTGTAGCAGTGAAATGCGTAGAGATCAGGAGGAACATCCATGGCGAAGGCAGCTACCTGGACCAACACTGACACTGAGGCACGAAAGCGTGGGGAGCAAACAGG'
+# seq = 'ATCGCGCGTTATATGGC'
 
 
 # qc = QC(path)
 t = Taxonomy()
 
-# min_e = 1
-# results = []
-#
-# blast = t.blast_search(seq)
-# for result in blast:
-#
-#     if result['e-value'] <= min_e:
-#         results.append(result)
-#         min_e = result['e-value']
-#
-#     print(result)
-#     print(type(result['hit']))
-#     print(type(result['length']))
-#     print(type(result['e-value']))
-#     print(type(result['sequence']))
-#
-# print(results)
+# TODO: after initial filter sort results per yield by: size
+# TODO: figure out how to access alignemnt len, % identity, coverage
+
 
 blast = t.blast_search(seq)
 for result in t.e_val_filter(blast):
@@ -36,31 +24,26 @@ for result in t.e_val_filter(blast):
     print(result['e-value'])
     print(result['sequence'])
 
-print("*"*3)
-print("*"*3)
-print("*"*3)
-
-hit = 'gi|2727575369|gb|PP762207.1| Stenotrophomonas maltophilia strain MRC55455 16S ribosomal RNA gene, partial sequence'
-
-subsec = hit.split('|')
-# GeneInfo ID (NCBI)
-gi = subsec[1]
-# GenBank db accession number
-gb = subsec[3]
-name = subsec[4].split(',')[0].strip()
-partial = True if 'partial sequence' == subsec[4].split(',')[1].strip() else False
 
 
+# hit = 'gi|2727575369|gb|PP762207.1| Stenotrophomonas maltophilia strain MRC55455 16S ribosomal RNA gene, partial sequence'
+#
+# subsec = hit.split('|')
+# # GeneInfo ID (NCBI)
+# gi = subsec[1]
+# # GenBank db accession number
+# gb = subsec[3]
+# name = subsec[4].split(',')[0].strip()
+# partial = True if 'partial sequence' == subsec[4].split(',')[1].strip() else False
+#
+#
+#
+# print(gi)
+# print(gb)
+# print(name)
+# print(partial)
 
-print(gi)
-print(gb)
-print(name)
-print(partial)
-
-"""                         'hit': alignment.title,
-                            'length': alignment.length,
-                            'e-value': hsp.expect,
-                            'sequence': hsp.sbjct
+"""
 *e - value - error
 prob
 *length - longer is better(context)
@@ -76,17 +59,5 @@ are
 context - strains
 mentioned
 """
-
-# seq = 'TACGAAGGGTGCAAGCGTTACTCGGAATTACTGGGCGTAAAGCGTGCGTAGGTGGTCGTTTAAGTCTGTTGTGAAAGCCCTGGGCTCAACCTGGGAACTGCAGTGGAAACTGGACGACTAGAGTGTGGTAGAGGGTAGCGGAATTCCTGGTGTAGCAGTGAAATGCGTAGAGATCAGGAGGAACATCCATGGCGAAGGCAGCTACCTGGACCAACACTGACACTGAGGCACGAAAGCGTGGGGAGCAAACAGG'
-# phreds = [32, 33, 32, 32, 18, 29, 32, 32, 32, 29, 29, 32, 38, 38, 38, 37, 17, 36, 36, 36, 37, 38, 39, 38, 38, 38, 32, 37, 38, 37, 39, 39, 39, 34, 17, 37, 36, 36, 38, 38, 39, 39, 38, 32, 16, 33, 36, 36, 38, 38, 34, 37, 38, 39, 38, 32, 18, 15, 15, 33, 33, 38, 38, 39, 39, 37, 20, 37, 19, 37, 38, 38, 36, 39, 37, 39, 33, 38, 39, 39, 17, 37, 38, 38, 37, 38, 37, 37, 34, 38, 37, 38, 37, 39, 37, 37, 39, 38, 39, 36, 39, 35, 19, 35, 38, 37, 38, 37, 38, 37, 39, 38, 36, 14, 33, 36, 39, 39, 39, 38, 39, 39, 39, 38, 27, 38, 39, 39, 39, 39, 39, 39, 39, 38, 38, 35, 33, 38, 37, 38, 39, 38, 38, 38, 39, 37, 39, 39, 36, 39, 37, 33, 38, 38, 37, 38, 35, 38, 38, 37, 39, 38, 36, 37, 36, 37, 35, 33, 33, 38, 35, 16, 38, 35, 37, 36, 39, 36, 39, 39, 38, 38, 37, 32, 33, 16, 33, 16, 14, 34, 38, 38, 39, 39, 39, 39, 39, 39, 39, 38, 33, 32, 16, 32, 39, 39, 37, 39, 38, 36, 32, 32, 32, 33, 17, 33, 16, 37, 37, 37, 37, 37, 38, 38, 34, 38, 33, 32, 15, 15, 36, 38, 36, 37, 37, 38, 38, 38, 38, 38, 38, 37, 37, 37, 33, 37, 37, 37, 32, 32, 32, 32, 32]
-
-# seq = 'ATCGCGCGTTATATGGC'
-
-
-
-
-
-
-
 
 
--- src/taxonomy.py
+++ src/taxonomy.py
@@ -7,7 +7,22 @@ from Bio.Blast import NCBIWWW, NCBIXML
 
 
 class Taxonomy:
+    def __init__(self):
+        self.min_e_val = 1
+
     def blast_search(self, seq: str) -> {}:
+        """
+        BLAST search on NCBI online nucleotide (nt) DB
+        Generator yields alignment hits as dict.
+        Eg:
+        {'hit': 'gi|2727575369|gb|PP762207.1| Stenotrophomonas maltophilia strain MRC55455 16S ribosomal RNA gene, partial sequence',
+         'length': 1403,
+         'e-value': 7.21649e-124,
+         'sequence': 'TACGAAGGGTGCAAGCGTTACTCGGAATTACTGGGCGTAAAGCGTGCGTAGGTGGTCGTTTAAGTCTGTTGTGAAAGCCCTGGGCTCAACCTGGGAACTGCAGTGGAAACTGGACGACTAGAGTGTGGTAGAGGGTAGCGGAATTCCTGGTGTAGCAGTGAAATGCGTAGAGATCAGGAGGAACATCCATGGCGAAGGCAGCTACCTGGACCAACACTGACACTGAGGCACGAAAGCGTGGGGAGCAAACAGG'
+        }
+        :param seq: str of nucleotide sequence
+        :return: Generator[Dict[str, Any], None, None] record hit from nt db
+        """
         try:
             result = NCBIWWW.qblast("blastn", "nt", seq, hitlist_size=5)
             records = NCBIXML.parse(result)
@@ -24,10 +39,13 @@ class Taxonomy:
             print(f"Error during blast on sequence {seq}: {e}")
 
     def e_val_filter(self, blast: Generator[dict, None, None]) -> {}:
-        min_e = 1
-        print("min e", min_e)
+        """
+        Utility method filters accepted BLAST results to current lowest e-value.
+        :param blast: Generator[Dict[str, Any], None, None] input from self.blast_search()
+        :return: Generator[Dict[str, Any], None, None] record hit from nt db
+        """
         for result in blast:
-            if result['e-value'] <= min_e:
-                min_e = result['e-value']
+            if result['e-value'] <= self.min_e_val:
+                self.min_e_val = result['e-value']
                 yield result
 
